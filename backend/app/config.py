8f2564d655d9e45f4627ac120a2bf1b383ea44be
Rohan Surve <rohansurve212@gmail.com>
2025-04-26 00:00:40 -0400

    Modified CORS ORIGINS env variable

--- backend/app/config.py
+++ backend/app/config.py
@@ -8,7 +8,7 @@ load_dotenv()
 class Settings(BaseSettings):
     APP_NAME: str = "Task Manager API"
     DATABASE_URL: str = os.getenv("DATABASE_URL", "sqlite:///./tasks.db")
-    CORS_ORIGINS: list = ["*"]
+    CORS_ORIGINS: str = "https://task-app-frontend-ay5oqkoyoa-ue.a.run.app"
     API_PREFIX: str = "/api"
 
     class Config:
